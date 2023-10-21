2e0c31a2e40c7fcc53eed1baca3846468300af76
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-10-21 14:30:17 +0530

    optimized for hosting

--- api/settings.py
+++ api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@xl=n+n4=o%o1aym7^yc388@t5#@xdp&#x07omjbg@j*snn0-6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
