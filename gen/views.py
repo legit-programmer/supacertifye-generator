796fd9c72ea02f897e55fb4781c097f7a91fd464
Siddique Khan <project.purpose.rollno.29@gmail.com>
2023-12-02 13:48:18 +0530

    added token based authorization

--- gen/views.py
+++ gen/views.py
@@ -4,6 +4,7 @@ from rest_framework.decorators import api_view
 from rest_framework import status, response
 from .supagen import *
 from PIL import UnidentifiedImageError
+from dotenv import load_dotenv
 
 # from.import fetch
 
@@ -15,28 +16,30 @@ from PIL import UnidentifiedImageError
 #         print("certificates generateed succesfully✅")
 #         return HttpResponse("generated")
 
-
+load_dotenv()
 def index(request):
     return HttpResponse('Grinding...🔨🔨🔨')
 
 @api_view(['POST'])
 def generate(request):
     data = dict(request.data)
-    
-    cords = {'name':data['name_cords'],
-             'class':data['class_cords'],
-             'eventname':data['eventname_cords'],
-             'date':data['date_cords'],
-             'position':data['postion_cords'],
-             }
-    template_url = data['template_url']    
-    try:
-        arr = supagenerate(data['event_id'], cords, template_url)
-        zipAndUpload(data['event_id'], arr)
-        # uploadAllToBucket(data['event_id'], arr)
+    print(data['token'])
+    if data['token'] == os.environ.get('REQUEST_TOKEN'):
+        cords = {'name':data['name_cords'],
+                'class':data['class_cords'],
+                'eventname':data['eventname_cords'],
+                'date':data['date_cords'],
+                'position':data['postion_cords'],
+                }
+        template_url = data['template_url']    
+        try:
+            arr = supagenerate(data['event_id'], cords, template_url)
+            zipAndUpload(data['event_id'], arr)
+            # uploadAllToBucket(data['event_id'], arr)
 
-    except Exception as e:
-        print(e)
-        return response.Response("Please check your request format or the template url you posted.", status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
+            print(e)
+            return response.Response("Please check your request format or the template url you posted.", status=status.HTTP_400_BAD_REQUEST)
 
-    return response.Response(data, status=status.HTTP_200_OK)
+        return response.Response(data, status=status.HTTP_200_OK)
+    return response.Response("Forbidden", status.HTTP_403_FORBIDDEN)
